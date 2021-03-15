7d452aa16708a492e44d8d4c46f0b5930ce0af51
TimOAO <t072000@gmail.com>
2021-03-15 19:39:37 +0800

    dynamic

--- hw.js
+++ hw.js
@@ -1,8 +1,14 @@
 var degree = 0;
+$(function(){
+$(window).resize(function(){
+$("body").css("min-height", ($(window).height>0.666666667*$(window).width)?"100vh":"66.6666667vw");
+}).resize();
+})
+//$(".body").height(screen.height>0.666666667*screen.width?"100vh":"66.6666667vw")
 $(".right-down").click(function(){
 	degree -= 90;
-	$(this).animate({left:"73.242vw", top:"239.203vh"}, 1500, "swing");
-	$(".right-down-2").animate({left:"72.917vw", top:"110.742vh"}, 1500, "swing");
+	$(this).animate({left:"73.242vw", top:"112.5vw"}, 1500, "swing");
+	$(".right-down-2").animate({left:"72.917vw", top:"52.083245vw"}, 1500, "swing");
 	$(".description").animate({left:"-79.62vw"}, 1500, "swing");
 	$(".description-2").animate({left:"10.42vw"}, 1500, "swing");
 	$(".figure").animate({rotate: degree}, 1500, "swing");
@@ -10,8 +16,8 @@ $(".right-down").click(function(){
 })
 $(".right-down-2").click(function(){
 	degree -= 90;	
-	$(this).animate({left:"73.242vw", top:"239.203vh"}, 1500, "swing");
-	$(".right-down").animate({left:"72.917vw", top:"110.742vh"}, 1500, "swing");
+	$(this).animate({left:"73.242vw", top:"112.5vw"}, 1500, "swing");
+	$(".right-down").animate({left:"72.917vw", top:"52.083245vw"}, 1500, "swing");
 	$(".description-2").animate({left:"-79.62vw"}, 1500, "swing");
 	$(".description").animate({left:"10.42vw"}, 1500, "swing");
 	$(".figure").animate({rotate: degree}, 1500, "swing");
